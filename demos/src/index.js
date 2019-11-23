c357b73a36a4cc70aab147baa86dc8a8af8e0b87
Oleksii Trekhleb <trehleb@gmail.com>
2019-11-23 13:55:57 +0200

    Cleaning up file contents.

--- demos/src/index.js
+++ demos/src/index.js
@@ -2,4 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootElement = document.getElementById('root');
+ReactDOM.render(<App />, rootElement);
