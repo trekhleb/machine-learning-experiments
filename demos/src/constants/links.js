2008328f3edb19f8b5b53327a7266eeeea6e3f04
Oleksii Trekhleb <trehleb@gmail.com>
2019-12-02 06:58:06 +0100

    Add JS models path.

--- demos/src/constants/links.js
+++ demos/src/constants/links.js
@@ -1,4 +1,3 @@
 export const MACHINE_LEARNING_EXPERIMENTS_GITHUB_URL = 'https://github.com/trekhleb/machine-learning-experiments';
-export const MACHINE_LEARNING_EXPERIMENTS_DEMO_URL = 'todo';
 
-export const MODELS_PATH = '../../experiments';
+export const MODELS_PATH = '/models';
