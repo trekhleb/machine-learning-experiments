a1278e225817183f082fc6297039444cac7b3310
Oleksii Trekhleb <trehleb@gmail.com>
2019-12-28 14:50:39 +0100

    Use black as primary color.

--- demos/src/constants/theme.js
+++ demos/src/constants/theme.js
@@ -3,20 +3,20 @@ import { createMuiTheme } from '@material-ui/core/styles';
 
 // @see: https://github.com/mui-org/material-ui/blob/master/examples/create-react-app/src/theme.js
 const theme = createMuiTheme({
-  // palette: {
-  //   primary: {
-  //     main: '#556cd6',
-  //   },
-  //   secondary: {
-  //     main: '#19857b',
-  //   },
-  //   error: {
-  //     main: red.A400,
-  //   },
-  //   background: {
-  //     default: '#fff',
-  //   },
-  // },
+  palette: {
+    primary: {
+      main: '#24292e',
+    },
+    // secondary: {
+    //   main: '#19857b',
+    // },
+    // error: {
+    //   main: red.A400,
+    // },
+    // background: {
+    //   default: '#fff',
+    // },
+  },
 });
 
 export default theme;
