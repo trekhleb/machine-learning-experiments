7b24aaa7ad7b76cd20b99862c5581a3311ddd095
Oleksii Trekhleb <trehleb@gmail.com>
2019-12-28 20:49:45 +0100

    Slidebale header.

--- demos/src/components/shared/Header.js
+++ demos/src/components/shared/Header.js
@@ -11,6 +11,8 @@ import Typography from '@material-ui/core/Typography';
 import Hidden from '@material-ui/core/Hidden';
 import Tooltip from '@material-ui/core/Tooltip';
 import HomeIcon from '@material-ui/icons/Home';
+import Slide from '@material-ui/core/Slide';
+import useScrollTrigger from '@material-ui/core/useScrollTrigger';
 import GitHubIcon from '@material-ui/icons/GitHub';
 
 import { HOME_ROUTE } from '../../constants/routes';
@@ -31,42 +33,44 @@ const useStyles = makeStyles((theme) => ({
 
 const Header = (): Node => {
   const classes = useStyles();
+  const scrollTrigger = useScrollTrigger();
 
   return (
     <>
-      <AppBar position="fixed">
-        <Toolbar>
-          <Typography variant="h6" className={classes.logoTypography} noWrap>
-            <MaterialLink component={RouterLink} to={HOME_ROUTE} className={classes.logoLink}>
-              <EmojiObjectsIcon style={{ marginBottom: '-4px' }} />
-              {' '}
-              <Hidden only={['xs']}>
-                Machine Learning Experiments
-              </Hidden>
-              <Hidden only={['sm', 'md', 'lg', 'xl']}>
-                ML Experiments
-              </Hidden>
-            </MaterialLink>
-          </Typography>
+      <Slide appear={false} direction="down" in={!scrollTrigger}>
+        <AppBar position="fixed">
+          <Toolbar>
+            <Typography variant="h6" className={classes.logoTypography} noWrap>
+              <MaterialLink component={RouterLink} to={HOME_ROUTE} className={classes.logoLink}>
+                <EmojiObjectsIcon style={{ marginBottom: '-4px' }} />
+                {' '}
+                <Hidden only={['xs']}>
+                  Machine Learning Experiments
+                </Hidden>
+                <Hidden only={['sm', 'md', 'lg', 'xl']}>
+                  ML Experiments
+                </Hidden>
+              </MaterialLink>
+            </Typography>
 
-          <Tooltip title="ML Experiments List">
-            <MaterialLink component={RouterLink} to={HOME_ROUTE} color="inherit">
-              <IconButton color="inherit">
-                <HomeIcon />
-              </IconButton>
-            </MaterialLink>
-          </Tooltip>
+            <Tooltip title="ML Experiments List">
+              <MaterialLink component={RouterLink} to={HOME_ROUTE} color="inherit">
+                <IconButton color="inherit">
+                  <HomeIcon />
+                </IconButton>
+              </MaterialLink>
+            </Tooltip>
 
-          <Tooltip title="ML Experiments on GitHub">
-            <MaterialLink href={MACHINE_LEARNING_EXPERIMENTS_GITHUB_URL} color="inherit">
-              <IconButton color="inherit">
-                <GitHubIcon />
-              </IconButton>
-            </MaterialLink>
-          </Tooltip>
-
-        </Toolbar>
-      </AppBar>
+            <Tooltip title="ML Experiments on GitHub">
+              <MaterialLink href={MACHINE_LEARNING_EXPERIMENTS_GITHUB_URL} color="inherit">
+                <IconButton color="inherit">
+                  <GitHubIcon />
+                </IconButton>
+              </MaterialLink>
+            </Tooltip>
+          </Toolbar>
+        </AppBar>
+      </Slide>
       <div className={classes.offset} />
     </>
   );
