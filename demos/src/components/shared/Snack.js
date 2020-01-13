2401ddfa9b186ae1d5d38c25c95cd0e963e32ae7
Oleksii Trekhleb <trehleb@gmail.com>
2020-01-13 08:03:22 +0100

    Add Snack.

---
+++ demos/src/components/shared/Snack.js
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from 'react';
+import type { Node } from 'react';
+import Snackbar from '@material-ui/core/Snackbar';
+import Alert from '@material-ui/lab/Alert';
+import type { Color } from '@material-ui/lab';
+
+const autoHideDuration = 6000;
+
+type SnackProps = {
+  message: ?string,
+  severity?: ?Color,
+};
+
+const defaultProps = {
+  severity: 'success',
+};
+
+const Snack = (props: SnackProps): Node => {
+  const { message, severity } = props;
+
+  const [snackbarOpen, setSnackbarOpen] = useState(true);
+  const [snackbarMessage, setSnackbarMessage] = useState(null);
+
+  const onClose = () => {
+    setSnackbarOpen(false);
+  };
+
+  useEffect(() => {
+    if (message) {
+      setSnackbarOpen(true);
+    }
+    setSnackbarMessage(message);
+  }, [message]);
+
+  if (!snackbarMessage) {
+    return null;
+  }
+
+  return (
+    <Snackbar
+      open={snackbarOpen}
+      autoHideDuration={autoHideDuration}
+      onClose={onClose}
+      anchorOrigin={{
+        vertical: 'bottom',
+        horizontal: 'center',
+      }}
+    >
+      <Alert
+        elevation={6}
+        variant="filled"
+        severity={severity || 'success'}
+        onClose={onClose}
+      >
+        {snackbarMessage}
+      </Alert>
+    </Snackbar>
+  );
+};
+
+Snack.defaultProps = defaultProps;
+
+export default Snack;
