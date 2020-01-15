bd16aec4276774901177b56bf3ca795d972b0c9d
Oleksii Trekhleb <trehleb@gmail.com>
2020-01-15 06:48:22 +0100

    Detecting objects.

--- demos/src/components/experiments/ObjectsDetection/ObjectsDetection.js
+++ demos/src/components/experiments/ObjectsDetection/ObjectsDetection.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import type { Node } from 'react';
 import Box from '@material-ui/core/Box';
 import { makeStyles } from '@material-ui/core/styles';
@@ -22,6 +22,8 @@ const experimentDescription = 'Detecting objects on the image or camera';
 const notebookUrl = `${ML_EXPERIMENTS_GITHUB_NOTEBOOKS_URL}/digits_recognition_mlp/digits_recognition_mlp.ipynb`;
 
 // const modelPath = `${ML_EXPERIMENTS_DEMO_MODELS_PATH}/digits_recognition_mlp/model.json`;
+const defaultCameraStreamSize = 300;
+const maxCameraStreamSize = 400;
 
 const useStyles = makeStyles(() => ({
   paper: {
@@ -32,6 +34,10 @@ const useStyles = makeStyles(() => ({
 const ObjectsDetection = (): Node => {
   const classes = useStyles();
 
+  const cameraStreamWrapper = useRef(null);
+
+  const [width, setWidth] = useState(defaultCameraStreamSize);
+  const [height, setHeight] = useState(defaultCameraStreamSize);
   const [model, setModel] = useState(null);
   const [boxes, setBoxes] = useState(null);
   const [errorMessage, setErrorMessage] = useState(null);
@@ -57,9 +63,17 @@ const ObjectsDetection = (): Node => {
 
   // Load the model.
   useEffect(() => {
+    if (cameraStreamWrapper.current && cameraStreamWrapper.current.offsetWidth) {
+      const size = Math.min(maxCameraStreamSize, cameraStreamWrapper.current.offsetWidth);
+      setWidth(size);
+      setHeight(size);
+      console.log({size});
+    }
+
     if (model) {
       return;
     }
+
     cocoSsd.load()
       .then((cocoSsdModel) => {
         setModel(cocoSsdModel);
@@ -81,15 +95,12 @@ const ObjectsDetection = (): Node => {
     );
   }
 
-  const width = 400;
-  const height = 400;
-
   const canvasStyle = {
     marginTop: -1 * height,
   };
 
   return (
-    <Box>
+    <Box ref={cameraStreamWrapper}>
       <Box mb={1}>
         Your camera stream appears here
       </Box>
@@ -97,6 +108,8 @@ const ObjectsDetection = (): Node => {
         <CameraStream
           width={width}
           height={height}
+          maxWidth={maxCameraStreamSize}
+          maxHeight={maxCameraStreamSize}
           onVideoFrame={onVideoFrame}
         />
       </Paper>
