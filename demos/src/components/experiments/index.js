f940a47da9a2fcfc0fb942f299141d667f87e2a1
Oleksii Trekhleb <trehleb@gmail.com>
2020-01-31 19:51:23 +0100

    Adding Image Classification demo.

--- demos/src/components/experiments/index.js
+++ demos/src/components/experiments/index.js
@@ -2,11 +2,13 @@ import type { ExperimentsMap } from './types';
 import DigitsRecognitionMLP from './DigitsRecognitionMLP/DigitsRecognitionMLP';
 import DigitsRecognitionCNN from './DigitsRecognitionCNN/DigitsRecognitionCNN';
 import ObjectsDetectionSSDLiteMobilenetV2 from './ObjectsDetectionSSDLiteMobilenetV2/ObjectsDetectionSSDLiteMobilenetV2';
+import ImageClassificationMobilenetV2 from './ImageClassificationMobilenetV2/ImageClassificationMobilenetV2';
 
 const experiments: ExperimentsMap = {
   DigitsRecognitionMLP,
   DigitsRecognitionCNN,
   ObjectsDetectionSSDLiteMobilenetV2,
+  ImageClassificationMobilenetV2,
 };
 
 export default experiments;
