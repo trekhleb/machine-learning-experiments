cea352e5dd1b6e23c0802f210a6c35f46ba1b129
Oleksii Trekhleb <trehleb@gmail.com>
2020-02-19 06:47:28 +0100

    Adding Rock-Paper-Scissors experiment.

--- demos/src/components/experiments/RockPaperScissorsCNN/RockPaperScissorsCNN.js
+++ demos/src/components/experiments/RockPaperScissorsCNN/RockPaperScissorsCNN.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import type { Node } from 'react';
 import Paper from '@material-ui/core/Paper';
 import { makeStyles } from '@material-ui/core/styles';
@@ -6,9 +6,7 @@ import Box from '@material-ui/core/Box';
 import PlayArrowIcon from '@material-ui/icons/PlayArrow';
 import Grid from '@material-ui/core/Grid';
 import Fab from '@material-ui/core/Fab';
-import Looks3Icon from '@material-ui/icons/Looks3';
-import LooksTwoIcon from '@material-ui/icons/LooksTwo';
-import LooksOneIcon from '@material-ui/icons/LooksOne';
+import Zoom from '@material-ui/core/Zoom';
 
 import cover from './cover.jpg';
 import { ML_EXPERIMENTS_DEMO_MODELS_PATH, ML_EXPERIMENTS_GITHUB_NOTEBOOKS_URL } from '../../../constants/links';
@@ -22,7 +20,22 @@ const notebookUrl = `${ML_EXPERIMENTS_GITHUB_NOTEBOOKS_URL}/rock_paper_scissors_
 
 const modelPath = `${ML_EXPERIMENTS_DEMO_MODELS_PATH}/rock_paper_scissors_cnn/model.json`;
 
-const countDownStart = 4;
+const gameStates = {
+  notStarted: 'notStarted',
+  inProgress: 'inProgress',
+  predicting: 'predicting',
+  finished: 'finished',
+};
+
+type GameState = $Values<typeof gameStates>;
+
+const flipVideoHorizontally = true;
+
+const countDownStart = 3;
+const countDownTimeout = 700;
+const countDownHeight = 100;
+
+const scoreDownWidth = 100;
 
 const canvasWidth = 300;
 const canvasHeight = 300;
@@ -39,6 +52,19 @@ const useStyles = makeStyles(() => ({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  paperSnapshot: {
+    width: canvasWidth,
+    height: canvasHeight,
+    overflow: 'hidden',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    boxShadow: 'none',
+    marginTop: `-${canvasHeight}px`,
+    position: 'absolute',
+    background: 'none',
+    zIndex: 10,
+  },
   choice: {
     width: canvasWidth,
     height: canvasHeight,
@@ -52,6 +78,8 @@ const useStyles = makeStyles(() => ({
     alignItems: 'center',
     justifyContent: 'center',
     alignSelf: 'stretch',
+    width: `${scoreDownWidth}px`,
+    height: `${countDownHeight}px`,
   },
   playerName: {
     display: 'flex',
@@ -60,6 +88,23 @@ const useStyles = makeStyles(() => ({
     fontSize: '20px',
     fontWeight: 450,
     marginBottom: '10px',
+  },
+  score: {
+    fontSize: '35px',
+    fontWeight: 400,
+    width: `${scoreDownWidth}px`,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  countDown: {
+    fontSize: '80px',
+    fontWeight: 500,
+    width: `${scoreDownWidth}px`,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: `${countDownHeight}px`,
   },
 }));
 
@@ -98,70 +143,123 @@ const choices: Choices = {
 };
 
 const RockPaperScissorsCNN = (): Node => {
-  const [computerChoice, setComputerChoice] = useState('');
+  const [computerChoice, setComputerChoice] = useState(null);
+  const [humanChoice, setHumanChoice] = useState(null);
+  const [humanScore, setHumanScore] = useState(0);
+  const [computerScore, setComputerScore] = useState(0);
   const [counter, setCounter] = useState(null);
+  const [videoFrame, setVideoFrame] = useState(null);
+  const [gameState, setGameState] = useState(gameStates.notStarted);
+
+  const snapshotCanvasRef = useRef(null);
 
   const classes = useStyles();
 
+  const renderVideoSnapshot = () => {
+    if (!snapshotCanvasRef || !snapshotCanvasRef.current) {
+      return;
+    }
+    const canvas: HTMLCanvasElement = snapshotCanvasRef.current;
+    const canvasContext: CanvasRenderingContext2D = canvas.getContext('2d');
+    canvasContext.drawImage(
+      videoFrame, 0, 0, canvas.width, canvas.height,
+    );
+  };
+
+  const clearVideoSnapshot = () => {
+    if (!snapshotCanvasRef || !snapshotCanvasRef.current) {
+      return;
+    }
+    const canvas: HTMLCanvasElement = snapshotCanvasRef.current;
+    const canvasContext: CanvasRenderingContext2D = canvas.getContext('2d');
+    canvasContext.clearRect(0, 0, canvas.width, canvas.height);
+  };
+
   const onVideoFrame = async (video?: ?HTMLVideoElement) => {
-    console.log('+++ On video frame');
+    setVideoFrame(video);
   };
 
   const onGameStart = () => {
     setCounter(countDownStart);
+    setComputerChoice(null);
+    setHumanChoice(null);
+    setGameState(gameStates.inProgress);
+    clearVideoSnapshot();
   };
 
   const onGameEnd = () => {
-    console.log('+++ GAME!');
+    const randomIndex: number = Math.floor(Math.random() * 3);
+    const computerRandomChoice: Choice = Object.values(choices)[randomIndex];
+    setComputerChoice(computerRandomChoice);
+    setGameState(gameStates.predicting);
+    renderVideoSnapshot();
   };
+
+  // Flip canvas horizontally if needed.
+  useEffect(() => {
+    if (!snapshotCanvasRef || !snapshotCanvasRef.current) {
+      return;
+    }
+    if (!flipVideoHorizontally) {
+      return;
+    }
+    const canvas: HTMLCanvasElement = snapshotCanvasRef.current;
+    const canvasContext: CanvasRenderingContext2D = canvas.getContext('2d');
+    canvasContext.translate(canvas.width, 0);
+    canvasContext.scale(-1, 1);
+  }, []);
 
   // Countdown effect.
   useEffect(() => {
     if (counter === null) {
+      // Nothing to count down.
       return;
     }
     if (counter === 0) {
+      // Countdown is finished.
       setCounter(null);
       onGameEnd();
       return;
     }
-    setTimeout(() => setCounter(counter - 1), 500);
+    // Do the next tick.
+    setTimeout(
+      () => setCounter(counter - 1),
+      countDownTimeout,
+    );
   }, [counter]);
 
   const cameraPaper = (
-    <>
-      <Box className={classes.playerName}>
-        <span role="img" aria-label="You">😎</span>
-        {' '}
-        You
-      </Box>
-      <Paper className={classes.paper}>
-        <CameraStream
-          width={canvasWidth}
-          height={canvasHeight}
-          onVideoFrame={onVideoFrame}
-          facingMode="user"
-          flipHorizontal
-        />
-      </Paper>
-    </>
+    <Paper className={classes.paper}>
+      <CameraStream
+        width={canvasWidth}
+        height={canvasHeight}
+        onVideoFrame={onVideoFrame}
+        facingMode="user"
+        flipHorizontal={flipVideoHorizontally}
+      />
+    </Paper>
+  );
+
+  const snapshotPaper = (
+    <Paper className={classes.paperSnapshot}>
+      <canvas
+        width={canvasWidth}
+        height={canvasHeight}
+        ref={snapshotCanvasRef}
+      />
+    </Paper>
   );
 
   const computerChoicePaper = (
-    <>
-      <Box className={classes.playerName}>
-        <span role="img" aria-label="Computer">🤖</span>
-        {' '}
-        Computer
-      </Box>
-      <Paper className={classes.paper}>
+    <Paper className={classes.paper}>
+      <Zoom in={!!computerChoice} timeout={{ enter: 200 }}>
         <Box className={classes.choice}>
           <span role="img" aria-label="Choice">
             {computerChoice && computerChoice.icon}
           </span>
         </Box>
-      </Paper>
-    </>
+      </Zoom>
+    </Paper>
   );
 
   const buttons = counter === null ? (
@@ -181,8 +279,14 @@ const RockPaperScissorsCNN = (): Node => {
     </Box>
   ) : null;
 
+  const score = (
+    <Box className={classes.score}>
+      {`${humanScore} : ${computerScore}`}
+    </Box>
+  );
+
   const countdown = counter !== null ? (
-    <Box>
+    <Box className={classes.countDown}>
       {counter}
     </Box>
   ) : null;
@@ -232,15 +336,27 @@ const RockPaperScissorsCNN = (): Node => {
       <Box>
         <Grid container spacing={4} alignItems="center" justify="flex-start">
           <Grid item>
+            <Box className={classes.playerName}>
+              <span role="img" aria-label="You">😎</span>
+              {' '}
+              You
+            </Box>
             {cameraPaper}
+            {snapshotPaper}
           </Grid>
 
           <Grid item>
+            {score}
             {buttons}
             {countdown}
           </Grid>
 
           <Grid item>
+            <Box className={classes.playerName}>
+              <span role="img" aria-label="Computer">🤖</span>
+              {' '}
+              Computer
+            </Box>
             {computerChoicePaper}
           </Grid>
         </Grid>
