5a9a3997e96847d8e8ef0e2f385a16db38427856
Oleksii Trekhleb <trehleb@gmail.com>
2020-02-21 08:11:50 +0100

    Upgrade package versions.

--- demos/src/constants/links.js
+++ demos/src/constants/links.js
@@ -1,7 +1,8 @@
-export const ML_EXPERIMENTS_GITHUB_URL = 'https://github.com/trekhleb/machine-learning-experiments';
+export const ML_EXPERIMENTS_GITHUB_REPO_NAME = 'machine-learning-experiments';
+export const ML_EXPERIMENTS_GITHUB_URL = `https://github.com/trekhleb/${ML_EXPERIMENTS_GITHUB_REPO_NAME}`;
 export const ML_EXPERIMENTS_GITHUB_NOTEBOOKS_URL = `${ML_EXPERIMENTS_GITHUB_URL}/blob/master/experiments`;
 
-export const ML_EXPERIMENTS_DEMO_MODELS_PATH = '/models';
+export const ML_EXPERIMENTS_DEMO_MODELS_PATH = `/${ML_EXPERIMENTS_GITHUB_REPO_NAME}/models`;
 
 export const NB_VIEWER_URL = 'https://nbviewer.jupyter.org/v2/gh';
 export const GOOGLE_COLAB_URL = 'https://colab.research.google.com/github';
--- demos/src/constants/routes.js
+++ demos/src/constants/routes.js
@@ -1,5 +1,8 @@
-export const HOME_ROUTE = '/';
+import { ML_EXPERIMENTS_GITHUB_REPO_NAME } from './links';
+
+export const ROOT_ROUTE = '/';
+export const HOME_ROUTE = `/${ML_EXPERIMENTS_GITHUB_REPO_NAME}`;
 
 export const EXPERIMENT_ID_PARAM = 'experiment_id';
-export const EXPERIMENTS_ROUTE = '/experiments';
+export const EXPERIMENTS_ROUTE = `/${ML_EXPERIMENTS_GITHUB_REPO_NAME}/experiments`;
 export const EXPERIMENT_ROUTE = `${EXPERIMENTS_ROUTE}/:${EXPERIMENT_ID_PARAM}`;
