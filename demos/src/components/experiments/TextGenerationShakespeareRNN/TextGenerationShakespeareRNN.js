fc00e19a3cbc2bdd8d3653fe814ee614925f15b1
Oleksii Trekhleb <trehleb@gmail.com>
2020-04-08 08:08:10 +0200

    Add Text-Generation experiment.

--- demos/src/components/experiments/TextGenerationShakespeareRNN/TextGenerationShakespeareRNN.js
+++ demos/src/components/experiments/TextGenerationShakespeareRNN/TextGenerationShakespeareRNN.js
@@ -6,8 +6,6 @@ import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import FormControl from '@material-ui/core/FormControl';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
 import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
 import LinearProgress from '@material-ui/core/LinearProgress';
@@ -129,21 +127,19 @@ const TextGenerationShakespeareRNN = (): Node => {
   ) : null;
 
   const generatedTextElement = generatedText ? (
-    <Card>
-      <CardContent>
-        <Typography variant="h5" component="h2">
-          Generated text
-        </Typography>
-        <Typography variant="body2" component="div">
-          <TextField
-            value={generatedText}
-            variant="outlined"
-            multiline
-            fullWidth
-          />
-        </Typography>
-      </CardContent>
-    </Card>
+    <Box>
+      <Typography variant="h6" component="h3">
+        Generated text
+      </Typography>
+      <Typography variant="body2" component="div">
+        <TextField
+          value={generatedText}
+          variant="outlined"
+          multiline
+          fullWidth
+        />
+      </Typography>
+    </Box>
   ) : null;
 
   if (!model) {
