0f5bbbaaffc3fda9ceae6c21f65d5eb26a3a5e0d
Oleksii Trekhleb <trehleb@gmail.com>
2020-04-16 06:44:29 +0200

    Adding Wikipedia Text Generation experiment.

--- demos/src/components/experiments/TextGenerationShakespeareRNN/TextGenerationShakespeareRNN.js
+++ demos/src/components/experiments/TextGenerationShakespeareRNN/TextGenerationShakespeareRNN.js
@@ -9,6 +9,7 @@ import {
   ML_EXPERIMENTS_GITHUB_NOTEBOOKS_URL,
 } from '../../../constants/links';
 import TextGenerator from '../../shared/TextGenerator';
+import modelVocabulary from './vocabulary';
 
 const experimentSlug = 'TextGenerationShakespeareRNN';
 const experimentName = 'Shakespeare Text Generation (RNN)';
@@ -17,15 +18,12 @@ const notebookUrl = `${ML_EXPERIMENTS_GITHUB_NOTEBOOKS_URL}/text_generation_shak
 
 const modelPath = `${ML_EXPERIMENTS_DEMO_MODELS_PATH}/text_generation_shakespeare_rnn/model.json`;
 
-const TextGenerationShakespeareRNN = (): Node => {
-  const modelVocabulary = ['\n', ' ', '!', '$', '&', "'", ',', '-', '.', '3', ':', ';', '?', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-  return (
-    <TextGenerator
-      modelPath={modelPath}
-      modelVocabulary={modelVocabulary}
-    />
-  );
-};
+const TextGenerationShakespeareRNN = (): Node => (
+  <TextGenerator
+    modelPath={modelPath}
+    modelVocabulary={modelVocabulary}
+  />
+);
 
 const experiment: Experiment = {
   slug: experimentSlug,
