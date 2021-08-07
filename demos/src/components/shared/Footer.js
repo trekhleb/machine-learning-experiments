b178947c65e69ce249f74470ed65f271bde7d027
Oleksii Trekhleb <trehleb@gmail.com>
2021-08-07 18:16:26 +0200

    Add a story for "Recipes generation".

--- demos/src/components/shared/Footer.js
+++ demos/src/components/shared/Footer.js
@@ -3,11 +3,11 @@ import React from 'react';
 import type { Node } from 'react';
 import Button from '@material-ui/core/Button';
 import Box from '@material-ui/core/Box';
-import BugReportIcon from '@material-ui/icons/BugReport';
+import MenuBookIcon from '@material-ui/icons/MenuBook';
 import GitHubIcon from '@material-ui/icons/GitHub';
 import Tooltip from '@material-ui/core/Tooltip';
 
-import { ML_EXPERIMENTS_GITHUB_URL, ML_EXPERIMENTS_GITHUB_ISSUES_URL } from '../../constants/links';
+import { ML_EXPERIMENTS_GITHUB_URL, TREKHLEB_DEV_URL } from '../../constants/links';
 
 const Footer = (): Node => (
   <>
@@ -25,13 +25,13 @@ const Footer = (): Node => (
       </Box>
 
       <Box>
-        <Tooltip title="Create an issue on GitHub">
+        <Tooltip title="More articles about machine learning and web development">
           <Button
             size="small"
-            startIcon={<BugReportIcon />}
-            onClick={() => window.open(ML_EXPERIMENTS_GITHUB_ISSUES_URL, '_blank')}
+            startIcon={<MenuBookIcon />}
+            onClick={() => window.open(TREKHLEB_DEV_URL, '_blank')}
           >
-            Report an issue
+            trekhleb.dev
           </Button>
         </Tooltip>
       </Box>
--- demos/src/constants/links.js
+++ demos/src/constants/links.js
@@ -3,6 +3,7 @@ export const ML_EXPERIMENTS_GITHUB_REPO_NAME = 'machine-learning-experiments';
 export const ML_EXPERIMENTS_GITHUB_URL = `https://github.com/trekhleb/${ML_EXPERIMENTS_GITHUB_REPO_NAME}`;
 export const ML_EXPERIMENTS_GITHUB_ISSUES_URL = `${ML_EXPERIMENTS_GITHUB_URL}/issues`;
 export const ML_EXPERIMENTS_GITHUB_NOTEBOOKS_URL = `${ML_EXPERIMENTS_GITHUB_URL}/blob/master/experiments`;
+export const TREKHLEB_DEV_URL = 'https://trekhleb.dev/blog';
 
 export const ML_EXPERIMENTS_DEMO_MODELS_PATH = `/${ML_EXPERIMENTS_GITHUB_REPO_NAME}/models`;
 
